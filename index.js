7deab56e0f77b10a771bf2e556cd8d8e1c268ae1
HiFamous11-com <80319540+HiFamous11-com@users.noreply.github.com>
2021-11-30 23:37:17 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+
+
+      var countDownDate = new Date("Dec 25, 2021 00:00:00").getTime();
+      
+      var countdownfunction = setInterval(function() {
+      
+        var now = new Date().getTime();
+        
+        var distance = countDownDate - now;
+        
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        
+        document.getElementById("demo").innerHTML = days + " days  ";
+        document.getElementById("demi").innerHTML =  hours + " hours  ";
+        document.getElementById("deml").innerHTML = minutes + " minutes  ";
+        document.getElementById("demg").innerHTML = seconds + " seconds";
+        document.getElementById("demb").innerHTML = days + " days  " + hours + " hours  " + minutes + " minutes  " + seconds + " seconds";
+        
+        if (distance < 0) {
+          clearInterval(countdownfunction);
+          document.getElementById("demo").innerHTML ="Happy Christmas!";
+
+          clearInterval(countdownfunction);
+          document.getElementById("demi").innerHTML ="Happy Christmas!";
+
+          clearInterval(countdownfunction);
+          document.getElementById("deml").innerHTML ="Happy Christmas!";
+
+          clearInterval(countdownfunction);
+          document.getElementById("demg").innerHTML ="Happy Christmas!";
+
+          clearInterval(countdownfunction);
+          document.getElementById("demb").innerHTML ="Happy Christmas!";
+        }
+      }, 1000);
