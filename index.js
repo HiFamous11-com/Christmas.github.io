73ef03b0109733321c506d93a0166c7587f2caa9
HiFamous11-com <80319540+HiFamous11-com@users.noreply.github.com>
2022-12-07 07:05:16 +0530

    lol

--- index.js
+++ index.js
@@ -1,37 +1,23 @@
+
 var countDownDate = new Date("Dec 25, 2022 00:00:00").getTime();
-      
-var countdownfunction = setInterval(function() {
+
+var x = setInterval(function() {
 
   var now = new Date().getTime();
-  
+    
   var distance = countDownDate - now;
-  
+    
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  
-  document.getElementById("demo").innerHTML = days + " days  ";
-  document.getElementById("demi").innerHTML =  hours + " hours  ";
-  document.getElementById("deml").innerHTML = minutes + " minutes  ";
-  document.getElementById("demg").innerHTML = seconds + " seconds";
-  document.getElementById("demb").innerHTML = days + " days  " + hours + " hours  " + minutes + " minutes  " + seconds + " seconds";
-  
+    
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
   if (distance < 0) {
-    clearInterval(countdownfunction);
-    document.getElementById("demo").innerHTML ="Happy Christmas!";
-
-    clearInterval(countdownfunction);
-    document.getElementById("demi").innerHTML ="Happy Christmas!";
-
-    clearInterval(countdownfunction);
-    document.getElementById("deml").innerHTML ="Happy Christmas!";
-
-    clearInterval(countdownfunction);
-    document.getElementById("demg").innerHTML ="Happy Christmas!";
-
-    clearInterval(countdownfunction);
-    document.getElementById("demb").innerHTML ="Happy Christmas!";
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "Happy Christmas! 🎄❤";
   }
 }, 1000);
 
